bcd4a4891c0c51ffe39297ee8a5f3e4bd2039a39
Edu <eduke@gmx.de>
2021-05-08 11:19:26 +0200

    reenable say hello

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv';
 const bot = new Geobot(dotenv.config().parsed.TOKEN);
 bot.start()
   .then(() => {
-    // bot.sayHello();
+    bot.sayHello();
   })
   .catch((error) => {
     console.log('Oh no! ', error);
